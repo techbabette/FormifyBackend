fe107a62aa082d04e8f24c6be392efbd23e72b2e
techbabette <marinakrsticrf@gmail.com>
2024-07-29 05:50:10 +0200

    [FEAT] Groups and group permission models

---
+++ application/app/Models/GroupPermission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupPermission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["permission", "group_id"];
+}
