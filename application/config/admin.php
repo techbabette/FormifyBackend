84fccad18f6299d6ae4e0927a997e2718fcddd2f
techbabette <marinakrsticrf@gmail.com>
2024-08-16 13:58:19 +0200

    [FEAT] Dynamic first user creation through environment variables

---
+++ application/config/admin.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'ADMIN_PASSWORD' => env('ADMIN_PASSWORD'),
+    'ADMIN_EMAIL' => env('ADMIN_EMAIL')
+];
+?>
--- application/database/seeders/UserSeeder.php
+++ application/database/seeders/UserSeeder.php
@@ -17,8 +17,8 @@ class UserSeeder extends Seeder
             [
                 'first_name' => 'First',
                 'last_name' => 'Last',
-                'email' => 'ilija.krstic.155.21@ict.edu.rs',
-                'password' => 'password',
+                'email' => config('admin.ADMIN_EMAIL'),
+                'password' => config('admin.ADMIN_PASSWORD'),
                 'email_verified_at' => now(),
                 'group_id' => 2
             ]
