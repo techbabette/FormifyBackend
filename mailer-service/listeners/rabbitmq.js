69b675c7e88e047e6d3d4c1b366a1b10beda17c7
techbabette <marinakrsticrf@gmail.com>
2024-09-25 10:59:23 +0200

    [FEAT] Mailer sends like to frontend activation page

--- mailer-service/listeners/rabbitmq.js
+++ mailer-service/listeners/rabbitmq.js
@@ -12,48 +12,49 @@ function rabbitmq(transporter){
         var exchange = 'main_exchange';
         var queue = "mail_queue"
         var event = "mail_token"
-  
-  
+
+
         channel.assertExchange(exchange, 'direct', {
           durable : true
         });
-  
+
         channel.assertQueue(queue, {
           exclusive: false
         }, function(error2, q){
           if (error2){
             throw error2;
           }
-  
+
           channel.prefetch(1);
-  
+
           console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-  
+
           channel.bindQueue(q.queue, exchange, event)
-  
+
           channel.consume(q.queue, data => {
             let message = JSON.parse(data.content.toString());
             let mailOptions = {
               to: message.email,
-              text: `Your activation token is ${message.token}`,
+              html: `Click <a href="${process.env.ACTIVATION_URL}/${message.token}">here to activate your acount</a>`,
+              text: `Click the following link to activate your account ${process.env.ACTIVATION_URL}/${message.token}`,
               subject: "Formify activation mail"
             }
-  
+
             transporter.sendMail(mailOptions, (error, info) => {
               if (error){
                 console.log(error.stack)
                 return channel.nack(data);
               }
-        
+
               console.log("Delivered message", info.messageId);
               channel.ack(data);
             });
-  
+
             channel.ack(data);
           });
         });
       });
     });
 }
-  
+
 module.exports = rabbitmq;
